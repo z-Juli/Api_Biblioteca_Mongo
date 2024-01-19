6b60a1196093c17648127f2f2e433818e662255f
Julian <julian.serantes06@gmail.com>
2024-01-19 18:40:09 -0300

    Version1

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+app.use(express.json());
+
+const librosRouter = require("./routes/libros");
+
+const errorHandler = require("./middleware/errorHandler")
+
+app.use('/libros', librosRouter);
+
+app.use(errorHandler);
+
+app.listen(3000, () => {
+    console.log('Servidor iniciado en el puerto 3000');
+});
+
